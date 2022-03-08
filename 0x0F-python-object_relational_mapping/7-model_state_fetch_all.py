bcd23bd7cb2dc85ae0eb6d5d16022aa140258754
sentayehut <tsentayehu4@gmail.com>
2022-03-08 00:53:17 -0800

    model state

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,34 +1,26 @@
 #!/usr/bin/python3
 """
-Script that lists all lists all `State`
-objects from the database `hbtn_0e_6_usa`.
-Arguments:
-    mysql username (str)
-    mysql password (str)
-    database name (str)
+script that lists all State objects from the database hbtn_0e_6_usa
+    - Your script should take 3 arguments: mysql username, mysql password
+    and database name
+    - uses the module SQLAlchemy
+    - imports State and Base from model_state
+    - script connects to a MySQL server running on localhost at port 3306
+    - Results are sorted in ascending order by states.id
 """
 
-import sys
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import Session
-from sqlalchemy.engine.url import URL
-from model_state import Base, State
+if __name__ == '__main__':
+    import sys
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
 
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    s = Session()
 
-if __name__ == "__main__":
-    mySQL_u = sys.argv[1]
-    mySQL_p = sys.argv[2]
-    db_name = sys.argv[3]
-
-    url = {'drivername': 'mysql+mysqldb', 'host': 'localhost',
-           'username': mySQL_u, 'password': mySQL_p, 'database': db_name}
-
-    engine = create_engine(URL(**url), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-
-    session = Session(bind=engine)
-
-    q = session.query(State).order_by(State.id)
-
-    for instance in q:
-        print("{}: {}".format(instance.id, instance.name))
+    for row in s.query(State).order_by(State.id.asc()):
+        print(row.id, end="")
+        print(': ', end="")
+        print(row.name)
